63541fad27a3ffa92ff481895817c36ff6d8cfa3
kiem.nguyen <nguyenhaanhkiem@gmail.com>
2026-09-14 22:16:50 +0700

    chore: add index export files for data-provider interfaces and response DTOs

--- src/modules/data-provider/dtos/responses/index.ts
+++ src/modules/data-provider/dtos/responses/index.ts
@@ -1,5 +1,5 @@
-export * from './discovery-session-summary-response.dto';
-export * from './ingest-discovered-url-response.dto';
+export * from './discovery-session-response.dto';
+export * from './discovery-url-response.dto';
 export * from './scrape-item-data-response.dto';
 export * from './scraping-data-response.dto';
 export * from './search-extract-data-response.dto';
--- src/modules/data-provider/interfaces/index.ts
+++ src/modules/data-provider/interfaces/index.ts
@@ -1,7 +1,6 @@
 export * from './data-provider-feature-service.interface';
 export * from './data-provider-scraper-service.interface';
 export * from './data-provider-search-service.interface';
-export * from './discovery-runner.interface';
 export * from './discovery-session.interface';
 export * from './scraper.interface';
 export * from './target-config.interface';
